11161a606d5ef33b385bd4931967fbb3cce09dab
MrWaoow <mr.waoow97@gmail.com>
2022-11-04 05:09:06 +0200

    Added all Website files

---
+++ assets/js/login_signup.js
@@ -0,0 +1,34 @@
+/*$(function() {
+    const loginBtn = document.getElementById("login-form-link");
+    const signupBtn = document.getElementById("signup-form-link");
+    const loginForm = document.getElementById("login-form");
+    const signupForm = document.getElementById("signup-form");
+
+    loginBtn.addEventListener("click", (event) => {
+        loginForm.delay(100).fadeIn(100);
+        signupForm.fadeOut(100);
+        signupBtn.classList.remove("active");
+        loginBtn.classList.add("active");
+        event.preventDefault();
+    });
+    signupBtn.addEventListener("click", (event) => {
+        signupForm.delay(100).fadeIn(100);
+        loginForm.fadeOut(100);
+        loginBtn.classList.remove("active");
+        signupBtn.classList.add("active");
+        event.preventDefault();
+    });
+
+});*/
+const loginBtn = document.getElementById("login-form-link");
+const signupBtn = document.getElementById("signup-form-link");
+const loginForm = document.getElementById("login-form");
+const signupForm = document.getElementById("signup-form");
+const onClickLoginBtn = () => {
+    signupBtn.classList.remove("active");
+    loginBtn.classList.add("active");
+};
+const onClickSignupBtn = () => {
+    loginBtn.classList.remove("active");
+    signupBtn.classList.add("active");
+};
