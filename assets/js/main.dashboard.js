f58ecff0ddae14841235ac6d9e1c8ba94c22e7d0
MrWaoow <mr.waoow97@gmail.com>
2022-11-05 08:11:32 +0200

    Updated Personal page

--- assets/js/main.dashboard.js
+++ assets/js/main.dashboard.js
@@ -83,5 +83,72 @@
     var dt = new Date();
     document.getElementById("datetime").innerHTML = (("0" + (dt.getMonth() + 1)).slice(-2)) + "." + (("0" + dt.getDate()).slice(-2)) + "  " + (("0" + dt.getHours()).slice(-2)) + ":" + (("0" + dt.getMinutes()).slice(-2)) + " GMT+2";
 
+    // IP Toggler
+    const toggleIp = $("#toggle-ip");
+    const loginPass = document.getElementById("login-pass");
+    const iP = document.getElementById("ip");
+    const spin = document.getElementById("spinner");
+    const myStyles = `    opacity: 0.7;visibility: visible;`;
+    const defaultStyles = `    opacity: 0;visibility: hidden;`;
+
+    const toggleIpOn = () => {
+        setTimeout(() => {
+            loginPass.classList.add("hidden");
+            iP.classList.remove("hidden");
+            spin.style.cssText = defaultStyles;
+        }, 5000);
+    };
+    const toggleIpOff = () => {
+
+        setTimeout(() => {
+            iP.classList.add("hidden");
+            loginPass.classList.remove("hidden");
+            spin.style.cssText = defaultStyles;
+        }, 5000);
+
+    };
+
+    toggleIp.change(() => {
+        if (iP.classList.contains("hidden")) {
+            spin.style.cssText = myStyles;
+            toggleIpOn();
+        } else {
+            spin.style.cssText = myStyles;
+            toggleIpOff();
+        }
+    });
+
+    //select Country
+    /*$('#demo-form').parsley().on('field:validated', function () {
+        var ok = $('.parsley-error').length === 0;
+        $('.bs-callout-info').toggleClass('hidden', !ok);
+        $('.bs-callout-warning').toggleClass('hidden', ok);
+    })
+        .on('form:submit', function () {
+            return false; // Don't submit form for this demo
+        });
+    $('select').selectize({
+        onInitialize: function () {
+            $("#select-country-selectized").attr("data-parsley-errors-container", "#errors");
+        }
+    });
+    var onClass = "on";
+    var showClass = "show";
+    $("input, .selectize-control ").bind("checkval", function () {
+        var label = $(this).parent().find('label');
+        if (this.value !== "") {
+            label.addClass(showClass);
+        } else {
+            label.removeClass(showClass);
+        }
+    }).on("keyup", function () {
+        $(this).trigger("checkval");
+    }).on("focus", function () {
+        $(this).prev("label").addClass(onClass);
+    }).on("blur", function () {
+        $(this).prev("label").removeClass(onClass);
+    }).trigger("checkval");*/
+
+
 })(jQuery);
 
