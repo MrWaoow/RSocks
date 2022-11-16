d8b9d85372ce6c8c13abebab6a2f11f3188f6a07
MrWaoow <mr.waoow97@gmail.com>
2022-11-16 03:08:25 +0200

    FAQ Page Was Created

--- assets/js/main.tap.js
+++ assets/js/main.tap.js
@@ -105,21 +105,51 @@ const changeBtn = (evt) => {
 const Smooth = {
     r180: "r-180",
     init: (evt) => {
-        let el = evt.currentTarget;
+        let el = evt.currentTarget.parentNode;
         if (!el.hasAttribute('data-height')) {
-            el.dataset.height = "7rem";
+            el.style.height = "auto";
+            const elementHeight = parseFloat($(el).height());
+            const paddingTop = parseFloat(window.getComputedStyle(el, null).getPropertyValue('padding-top'));
+            const paddingBottom = parseFloat(window.getComputedStyle(el, null).getPropertyValue('padding-bottom'));
+            const borderTop = parseFloat(window.getComputedStyle(el, null).getPropertyValue('border-top'));
+            const borderBottom = parseFloat(window.getComputedStyle(el, null).getPropertyValue('border-bottom'));
+            const marginTop = parseFloat(window.getComputedStyle(el, null).getPropertyValue('margin-top'));
+            const marginBottom = parseFloat(window.getComputedStyle(el, null).getPropertyValue('margin-bottom'));
+            const fullHeight = elementHeight + paddingTop + paddingBottom + borderTop + borderBottom + marginTop + marginBottom;
+            const fullHeightToRem = parseFloat(fullHeight * 0.0625);
+            el.dataset.height = fullHeightToRem + "rem";
+            console.log(parseFloat(fullHeight));
             el.style.height = "3.5rem";
         }
         return el;
     },
     expandCollapse: (evt) => {
-        const icon = evt.currentTarget.childNodes[1].childNodes[3];
-        if (!icon.classList.contains(Smooth.r180)) {
+        const element = evt.currentTarget;
+        const icon = element.querySelector('.icon-sm');
+        const iconAnimation = Smooth.r180;
+        if (!icon.classList.contains(iconAnimation)) {
             icon.classList.add(Smooth.r180)
             let div1 = Smooth.init(evt);
             setTimeout(() => { div1.style.height = div1.dataset.height; }, 20);
         } else {
             icon.classList.remove(Smooth.r180)
+            let div2 = Smooth.init(evt);
+            div2.style.height = "3.5rem";
+        }
+    },
+    r90: "r-90",
+    expandCollapsePlus: (evt) => {
+        const element = evt.currentTarget;
+        const icon = element.querySelector('.icon-md');
+        const plus = icon.querySelector('.plus')
+        const iconAnimation = Smooth.r90;
+        if (plus.classList.contains(iconAnimation)) {
+            plus.classList.remove(Smooth.r90)
+            let div1 = Smooth.init(evt);
+            setTimeout(() => { div1.style.height = div1.dataset.height; }, 20);
+
+        } else {
+            plus.classList.add(Smooth.r90)
             let div2 = Smooth.init(evt);
             div2.style.height = "3.5rem";
         }
