3b9549cd344d65c2b7a01248e92eeb275b046118
MrWaoow <mr.waoow97@gmail.com>
2022-11-27 03:58:56 +0200

    dashboard.personal was updated

--- assets/js/main.dashboard.js
+++ assets/js/main.dashboard.js
@@ -3,6 +3,64 @@ Template Name: RSocks - Startup & SaaS Bootstrap 5 Template.
 Author: Mr. Waoow
 */
 
+const toggleIp = $("#toggle-ip");
+const loginPassContainer = document.getElementById("LoginPassContainer");
+const iPContainer = document.getElementById("IpContainer");
+const loginPass = document.getElementById("login-pass");
+const iP = document.getElementById("ip");
+const labelContainer = document.getElementById("LabelContainer");
+const formContainer = document.getElementById("FormContainer");
+const spin = document.getElementById("spinner");
+const myStyles = `    opacity: 0.7;visibility: visible;`;
+const defaultStyles = `    opacity: 0;visibility: hidden;`;
+
+// Switch to form container
+const showHideFormContainer = () => {
+    if (labelContainer.classList.contains("hidden")) {
+        labelContainer.classList.remove("hidden");
+        formContainer.classList.add("hidden");
+    } else {
+        formContainer.classList.remove("hidden");
+        labelContainer.classList.add("hidden");
+    }
+};
+
+// IP Toggler
+const toggleIpOn = () => {
+    setTimeout(() => {
+        loginPass.classList.add("hidden");
+        iP.classList.remove("hidden");
+        loginPassContainer.classList.add("hidden");
+        iPContainer.classList.remove("hidden");
+        labelContainer.classList.remove("hidden");
+        formContainer.classList.add("hidden");
+        spin.style.cssText = defaultStyles;
+    }, 5000);
+};
+const toggleIpOff = () => {
+
+    setTimeout(() => {
+        iP.classList.add("hidden");
+        loginPass.classList.remove("hidden");
+        iPContainer.classList.add("hidden");
+        loginPassContainer.classList.remove("hidden");
+        labelContainer.classList.remove("hidden");
+        formContainer.classList.add("hidden");
+        spin.style.cssText = defaultStyles;
+    }, 5000);
+
+};
+
+toggleIp.change(() => {
+    if (iP.classList.contains("hidden")) {
+        spin.style.cssText = myStyles;
+        toggleIpOn();
+    } else {
+        spin.style.cssText = myStyles;
+        toggleIpOff();
+    }
+});
+
 (($) => {
     "use strict";
 
@@ -57,105 +115,21 @@ Author: Mr. Waoow
         event.stopPropagation();
     });
 
-
-
-    // Progress Bar
-    $('.pg-bar').waypoint(function () {
-        $('.progress .progress-bar').each(function () {
-            $(this).css("width", $(this).attr("aria-valuenow") + '%');
-        });
-    }, { offset: '80%' });
-
-
-    // Calender
-    $('#calender').datetimepicker({
-        inline: true,
-        format: 'L'
-    });
-
-
-    // Testimonials carousel
-    $(".testimonial-carousel").owlCarousel({
-        autoplay: true,
-        smartSpeed: 1000,
-        items: 1,
-        dots: true,
-        loop: true,
-        nav: false
-    });
-
     //Time and Date
     var dt = new Date();
-    document.getElementById("datetime").innerHTML = (("0" + (dt.getMonth() + 1)).slice(-2)) + "." + (("0" + dt.getDate()).slice(-2)) + "  " + (("0" + dt.getHours()).slice(-2)) + ":" + (("0" + dt.getMinutes()).slice(-2)) + " GMT+2";
+    if (document.getElementById("datetime")) {
+        document.getElementById("datetime").innerHTML = (("0" + (dt.getMonth() + 1)).slice(-2)) + "." + (("0" + dt.getDate()).slice(-2)) + "  " + (("0" + dt.getHours()).slice(-2)) + ":" + (("0" + dt.getMinutes()).slice(-2)) + " GMT+2";
+    }
+    //Login, pass, op lable
+    const username = document.getElementById("Usernaem");
+    const password = document.getElementById("Password");
+    const Ip = document.getElementById("IP");
+    const userLabel = document.getElementById("UserLabel");
+    const passLabel = document.getElementById("PassLabel");
+    const IpLabel = document.getElementById("IpLabel");
 
-    // IP Toggler
-    const toggleIp = $("#toggle-ip");
-    const loginPass = document.getElementById("login-pass");
-    const iP = document.getElementById("ip");
-    const spin = document.getElementById("spinner");
-    const myStyles = `    opacity: 0.7;visibility: visible;`;
-    const defaultStyles = `    opacity: 0;visibility: hidden;`;
-
-    const toggleIpOn = () => {
-        setTimeout(() => {
-            loginPass.classList.add("hidden");
-            iP.classList.remove("hidden");
-            spin.style.cssText = defaultStyles;
-        }, 5000);
-    };
-    const toggleIpOff = () => {
-
-        setTimeout(() => {
-            iP.classList.add("hidden");
-            loginPass.classList.remove("hidden");
-            spin.style.cssText = defaultStyles;
-        }, 5000);
-
-    };
-
-    toggleIp.change(() => {
-        if (iP.classList.contains("hidden")) {
-            spin.style.cssText = myStyles;
-            toggleIpOn();
-        } else {
-            spin.style.cssText = myStyles;
-            toggleIpOff();
-        }
-    });
-
-    //select Country
-    /*$('#demo-form').parsley().on('field:validated', function () {
-        var ok = $('.parsley-error').length === 0;
-        $('.bs-callout-info').toggleClass('hidden', !ok);
-        $('.bs-callout-warning').toggleClass('hidden', ok);
-    })
-        .on('form:submit', function () {
-            return false; // Don't submit form for this demo
-        });
-    $('select').selectize({
-        onInitialize: function () {
-            $("#select-country-selectized").attr("data-parsley-errors-container", "#errors");
-        }
-    });
-    var onClass = "on";
-    var showClass = "show";
-    $("input, .selectize-control ").bind("checkval", function () {
-        var label = $(this).parent().find('label');
-        if (this.value !== "") {
-            label.addClass(showClass);
-        } else {
-            label.removeClass(showClass);
-        }
-    }).on("keyup", function () {
-        $(this).trigger("checkval");
-    }).on("focus", function () {
-        $(this).prev("label").addClass(onClass);
-    }).on("blur", function () {
-        $(this).prev("label").removeClass(onClass);
-    }).trigger("checkval");*/
-
-
-
+    userLabel.innerHTML = username.value;
+    passLabel.innerHTML = password.value;
+    IpLabel.innerHTML = Ip.value;
 
 })(jQuery);
-
